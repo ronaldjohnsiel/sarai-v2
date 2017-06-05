462d707720267da8276f0927fe03e826fec6ce15
Monina Carandang <reactor04@gmail.com>
2017-06-05 23:22:34 +0800

    Added upload packages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,15 +2,16 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
-import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
+// import '../../ui/layouts/body/body.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // MAIN LAYOUT
 import '../../ui/layouts/main/main-layout.js';
 
 // PAGES
+import '../../ui/pages/home/home.js';
 import '../../ui/pages/about/about.js';
+import '../../ui/pages/cms/cms.js';
 
 
 // Set up all routes in the app
@@ -30,6 +31,12 @@ FlowRouter.route('/about', {
   },
 });
 
+FlowRouter.route('/admin', {
+  name: 'admin.dashboard',
+  action() {
+    BlazeLayout.render('MainLayout', { main: 'CMS' });
+  },
+});
 // 404
 FlowRouter.notFound = {
   action() {
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,3 +2,18 @@
 
 import './fixtures.js';
 import './register-api.js';
+
+import { Random } from 'meteor/random';
+
+Meteor.startup(function() {
+
+  UploadServer.init({
+      tmpDir: '/opt/uploads/tmp',
+      uploadDir: '/opt/uploads/',
+      checkCreateDirectories: true, //create the directories for you
+      getFileName: function(fileInfo, formData) {
+        return Random.id()+".jpg";
+      }
+  });
+
+});
