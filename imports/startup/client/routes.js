5fd963949b098902c37a5df0f79183aff9931090
Monina Carandang <reactor04@gmail.com>
2017-06-05 19:29:53 +0800

    First commit. Scaffold project with minor edits

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,38 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Import needed templates
+import '../../ui/layouts/body/body.js';
+import '../../ui/pages/home/home.js';
+import '../../ui/pages/not-found/not-found.js';
+
+// MAIN LAYOUT
+import '../../ui/layouts/main/main-layout.js'
+
+// PAGES
+import '../../ui/pages/about/about.js';
+
+
+// Set up all routes in the app
+// HOMEPAGE
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    BlazeLayout.render('MainLayout', { main: 'App_home' });
+  }
+});
+
+// ABOUT
+FlowRouter.route('/about', {
+  name: 'about',
+  action() {
+    BlazeLayout.render('MainLayout', { main: 'About' });
+  }
+});
+
+// 404
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_notFound' });
+  },
+};
---
+++ imports/ui/layouts/main/main-layout.js
@@ -0,0 +1,4 @@
+import './main-layout.html';
+
+import '../../components/layout/main-footer.js';
+import '../../components/layout/main-navbar.js';
