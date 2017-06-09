ad31882c4757c2baa5f0fb4dfd890a4b5d5a9284
Vanessa Romero <vanessamaelromero@gmail.com>
2017-06-09 15:08:31 +0800

    Initial retrieve for cms

--- imports/api/weather/server/publications.js
+++ imports/api/weather/server/publications.js
@@ -5,17 +5,9 @@ import { WeatherData } from '../sarai-weather-data.js';
 import { HeatMapData } from '../sarai-heat-map-data.js';
 import { WeatherStations } from '../sarai-weather-stations.js';
 
-<<<<<<< 8dec776492af03e33108d8f2e7bf9a2dec6dd8c0
 Meteor.publish('weather_stations', function() {
   return WeatherStations.find();
 });
-=======
-
-Meteor.publish('weather_stations', function() {
-  return WeatherStations.find();
-});
-
->>>>>>> updated fixtures
 Meteor.publish('weather', function() {
   return Weather.find();
 });
@@ -32,7 +24,6 @@ Meteor.publish('heat_map_data', function() {
   return HeatMapData.find();
 });
 
-<<<<<<< 8dec776492af03e33108d8f2e7bf9a2dec6dd8c0
 Meteor.publish('weather-data-30', () => {
   const oneMonthAgo = new Date()
 
@@ -60,5 +51,3 @@ Meteor.publish('weather-data-30-by-id', (stationID) => {
 
   return WeatherData.find({id: stationID, dateUTC: { $gt: oneMonthAgo}}, {sort: {dateUTC: -1}});
 });
-=======
->>>>>>> updated fixtures
