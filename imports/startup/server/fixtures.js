cc6fd370e53eaa074008116948f547f6a382682b
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-13 11:20:49 +0800

    fixed merge conflicts

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -5911,7 +5911,7 @@ Meteor.startup(() => {
     data.forEach(slide => Slides.insert(slide));
   }
 
-  if (Services.find().count() == 0) {
+  if (Projects.find().count() == 0) {
   const data = [
     {
       name: 'suitability-maps',
