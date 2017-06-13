1bb8e35062decbddba8a3338a803e70f226c554d
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-13 11:20:49 +0800

    fixed merge conflicts

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -434,7 +434,7 @@ Meteor.startup(() => {
     data.forEach(slide => Slides.insert(slide));
   }
 
-  if (Services.find().count() == 0) {
+  if (Projects.find().count() == 0) {
   const data = [
     {
       url: '#',
