c451f4da349b1683d87b409790d5e0fafea9f17b
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-14 15:13:27 +0800

    Added functionality in Header Options

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -7392,7 +7392,7 @@ Meteor.startup(() => {
     data.forEach(slide => Slides.insert(slide));
   }
 
-  if (Projects.find().count() == 0) {
+  if (Services.find().count() == 0) {
   const data = [
     {
       name: 'suitability-maps',
@@ -7499,8 +7499,8 @@ Meteor.startup(() => {
       solution: 'Using the SARAi Nutrient Manager, registered farmers can get recommendation...',
       thumbnail: 'http://proptera.com/application/files/2314/2420/9499/iStock_000012551695_Lettuce_Large.jpg'
     }
-  ]
-    data.forEach(project => Projects.insert(project));
+  ];
+    data.forEach(project => Services.insert(project));
 
   }
 
