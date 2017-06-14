094d842de98517b1ef970ef70ff274498a5ea5f6
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-14 15:13:27 +0800

    Added functionality in Header Options

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1681,7 +1681,7 @@ Meteor.startup(() => {
     data.forEach(slide => Slides.insert(slide));
   }
 
-  if (Projects.find().count() == 0) {
+  if (Services.find().count() == 0) {
   const data = [
     {
       name: 'suitability-maps',
