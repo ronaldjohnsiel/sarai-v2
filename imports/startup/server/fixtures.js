38776338fca9914ec93cbff21fe2bd67d7a4d815
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-14 15:13:27 +0800

    Added functionality in Header Options

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1453,12 +1453,11 @@ Meteor.startup(() => {
       rank: 4,
       image: 'https://geneticliteracyproject.org/wp-content/uploads/2016/07/n-fix-crops.jpg',
     },
-
   ];
     data.forEach(slide => Slides.insert(slide));
   }
 
-  if (Projects.find().count() == 0) {
+  if (Services.find().count() == 0) {
   const data = [
     {
       name: 'suitability-maps',
