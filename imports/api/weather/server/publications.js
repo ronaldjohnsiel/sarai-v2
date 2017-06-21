49c76dc081a676455b627969d3e54e05608cedd3
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-21 16:58:10 +0800

    Crud for main options

--- imports/api/weather/server/publications.js
+++ imports/api/weather/server/publications.js
@@ -5,7 +5,6 @@ import { WeatherData } from '../sarai-weather-data.js';
 import { HeatMapData } from '../sarai-heat-map-data.js';
 import { WeatherStations } from '../sarai-weather-stations.js';
 
-
 Meteor.publish('weather_stations', function() {
   return WeatherStations.find();
 });
