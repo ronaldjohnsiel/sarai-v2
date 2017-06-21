4b33c6a634bd2e6e6f86758b8ab007f0980acdfa
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-21 16:58:10 +0800

    Crud for main options

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -66,6 +66,9 @@ import '../../api/main/server/publications.js';
 import '../../api/main/methods.js';
 import '../../api/main/server/publications.js';
 
+import '../../api/main/methods.js';
+import '../../api/main/server/publications.js';
+
 import '../../api/advisories/methods.js';
 import '../../api/advisories/server/publications.js';
 
