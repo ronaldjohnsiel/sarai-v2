8f4e337d67785d27e52c514f236f90c1567ef706
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-22 10:47:34 +0800

    updated fixtures

--- imports/ui/components/layout/cms-navbar.js
+++ imports/ui/components/layout/cms-navbar.js
@@ -1,10 +1,5 @@
 import { Links } from '/imports/api/links/links.js';
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base'
 
 import './cms-navbar.html';
 
-
-Accounts.config({
-  forbidClientAccountCreation: true,
-});
