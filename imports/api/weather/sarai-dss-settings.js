3c772cd347f6d12b4f5454084dc1618a88f11298
ronaldjohnsiel <rjleis9@gmail.com>
2017-06-22 10:47:34 +0800

    updated fixtures

--- imports/api/weather/sarai-dss-settings.js
+++ imports/api/weather/sarai-dss-settings.js
@@ -1,3 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 
+<<<<<<< 8dec776492af03e33108d8f2e7bf9a2dec6dd8c0
 export const DSSSettings = new Mongo.Collection('dss_settings');
+=======
+export const DSSSettings = new Mongo.Collection('dss_settings');
+>>>>>>> updated fixtures
--- imports/api/weather/server/publications.js
+++ imports/api/weather/server/publications.js
@@ -5,9 +5,17 @@ import { WeatherData } from '../sarai-weather-data.js';
 import { HeatMapData } from '../sarai-heat-map-data.js';
 import { WeatherStations } from '../sarai-weather-stations.js';
 
+<<<<<<< 8dec776492af03e33108d8f2e7bf9a2dec6dd8c0
 Meteor.publish('weather_stations', function() {
   return WeatherStations.find();
 });
+=======
+
+Meteor.publish('weather_stations', function() {
+  return WeatherStations.find();
+});
+
+>>>>>>> updated fixtures
 Meteor.publish('weather', function() {
   return Weather.find();
 });
@@ -24,6 +32,7 @@ Meteor.publish('heat_map_data', function() {
   return HeatMapData.find();
 });
 
+<<<<<<< 8dec776492af03e33108d8f2e7bf9a2dec6dd8c0
 Meteor.publish('weather-data-30', () => {
   const oneMonthAgo = new Date()
 
@@ -51,3 +60,5 @@ Meteor.publish('weather-data-30-by-id', (stationID) => {
 
   return WeatherData.find({id: stationID, dateUTC: { $gt: oneMonthAgo}}, {sort: {dateUTC: -1}});
 });
+=======
+>>>>>>> updated fixtures
