ef220a475a38be4e9f912781362e5344577d6ffb
Monina Carandang <reactor04@gmail.com>
2017-06-23 12:53:11 +0800

    Added highcharts to header, and a sample chart

--- imports/ui/pages/monitoring/monitoring.js
+++ imports/ui/pages/monitoring/monitoring.js
@@ -2,6 +2,12 @@ import './monitoring.html';
 
 Template.Monitoring.onCreated(function() {
 
+  Highcharts.setOptions({
+  // This is for all plots, change Date axis to local timezone
+      global : {
+          useUTC : false
+      }
+  });
 });
 
 Template.Monitoring.onRendered(function() {
@@ -30,6 +36,57 @@ Template.Monitoring.onRendered(function() {
     id: 'mapbox://styles/mcarandang/cj1jd9bo2000a2speyi8o7cle',
     accessToken: 'pk.eyJ1IjoibWNhcmFuZGFuZyIsImEiOiJjaWtxaHgzYTkwMDA4ZHZtM3E3aXMyYnlzIn0.x63VGx2C-BP_ttuEsn2fVg'
   }).addTo(weatherMap);
+
+  var s = $('#meteogram-container');
+  console.log(s);
+
+
+  // NOTE THAT THIS IS JUST A SAMPLE FROM THE HIGHCHARTS SITE
+  Highcharts.chart('meteogram-container', {
+
+    title: {
+        text: 'Solar Employment Growth by Sector, 2010-2016'
+    },
+
+    subtitle: {
+        text: 'Source: thesolarfoundation.com'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Number of Employees'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Installation',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Manufacturing',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Sales & Distribution',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Project Development',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }, {
+        name: 'Other',
+        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+    }]
+
+});
 
 });
 
