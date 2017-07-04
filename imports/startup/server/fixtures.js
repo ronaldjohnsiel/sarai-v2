5070ac847cc62a2e0c29067929f9f09cc5f32956
ronaldjohnsiel <rjleis9@gmail.com>
2017-07-04 14:32:09 +0800

    fixed upload

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1605,17 +1605,6 @@ Meteor.startup(() => {
     data.forEach(logo => Story.insert(logo));
   }
 
-  if (Reports.find().count() === 0) {
-    const data = [
-      // {
-      //   title:
-      //   file:
-      //   thumbnail:
-      //   date:
-      // }
-    ];
-  }
-
   if (Advisories.find().count() === 0){
     const data = [
       {
